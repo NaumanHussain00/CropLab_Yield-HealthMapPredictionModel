d542edcf3efd1e1f1c6bc07c20134cd06295cc4d
Nauman Hussian <nauman05.vivo@gmail.com>
2026-02-20 22:48:26 +0530

    Update CORS middleware configuration to allow all origins and methods

--- app.py
+++ app.py
@@ -24,9 +24,9 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[""],  # Allows all origins
+    allow_origins=["*"],  # Allows all origins
     allow_credentials=True,
-    allow_methods=[""],  # Allows all methods including OPTIONS
+    allow_methods=["*"],  # Allows all methods including OPTIONS
     allow_headers=["*"],  # Allows all headers
 )
 
